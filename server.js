abc4b5d70fd6f8e07180ff1fbfa89f01dae1f44d
Roman Jędras <romanjedras@o2.pl>
2019-07-15 18:34:42 +0200

    Add another port

--- server.js
+++ server.js
@@ -172,7 +172,7 @@ Promise.all([kenny.save(), mark.save(), benny.save()])
 .then(findBennyAndRemove)
 .catch(console.log.bind(console))
 
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3000;
 
  app.listen(port,'127.0.0.1',()=>{
  	console.log(`App running on port ${port}...`);
