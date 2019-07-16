0f6188782a6cd1daed49702a25a3ebbbd4fb1ec0
Roman Jędras <romanjedras@o2.pl>
2019-07-16 11:39:22 +0200

    Add web page to task

--- server.js
+++ server.js
@@ -1,9 +1,63 @@
 const mongoose = require('mongoose');
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
 const dotenv = require('dotenv');
 const Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './.gitignore' });
+
+const replaceTemplate = require('./modules/replaceTemplate');
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+const dataObj = JSON.parse(data);
+
+
+const server = http.createServer((req, res) => {
+	const {query, pathname} = url.parse(req.url, true);
+	
+	if (pathname === '/' || pathname === '/overview') {
+		
+		res.writeHead('200', {
+			'Content-type': 'text/html',
+		});
+		
+		const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+		const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+		res.end(output)
+		
+		
+		// Product page
+	} else if (pathname === '/product') {
+		res.writeHead('200', {
+			'Content-type': 'text/html',
+		});
+		const product = dataObj[query.id];
+		const output = replaceTemplate(tempProduct, product);
+		res.end(output)
+		
+	} else if (pathname === '/api') {
+		res.writeHead('200', {
+			'Content-type': 'application/json',
+		});
+		res.end(data);
+		
+	} else {
+		res.writeHead('404', {
+			'Content-type': 'text/html',
+			'my-own-header': 'hello-world'
+		});
+		res.end("<h1>404: Page not found</h1>")
+	}
+});
+
+
+
 
 const DB = process.env.DATABASE.replace('' + '<dbuser>', process.env.USENAME).replace(''+'<dbpassword>',process.env.DATABASE_PASSWORD);
 
